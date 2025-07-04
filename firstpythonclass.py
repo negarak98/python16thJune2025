6d48dfda8702edd2c77cbf236af7ae5b9ee6317b
negarak <negar.khatamian@yahoo.com>
2025-07-04 13:00:12 -0400

    commit11

--- firstpythonclass.py
+++ firstpythonclass.py
@@ -127,3 +127,17 @@ dinner = get_calories("dinner")
 total = breakfast + lunch + dinner
 
 print(f"\nTotal Calories for the Day: {total:.2f} kcal")
+
+
+
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius}°C is {fahrenheit}°F")
+
+word = input("Enter a word: ").strip().lower()
+
+if word and word[0] in "aeiou":
+    print(f"'{word}' starts with a vowel.")
+else:
+    print(f"'{word}' does not start with a vowel.")
+    
