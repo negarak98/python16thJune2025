0b7634506f3f9b40a89218dc7f77cd7f43329a0d
negarak <negar.khatamian@yahoo.com>
2025-07-04 12:03:49 -0400

    secondcommint

--- firstpythonclass.py
+++ firstpythonclass.py
@@ -1,2 +1,26 @@
 
 print("hello world")
+print ( [1,2,3,4,5,6,7,8,9,10])
+
+name = input ("what is your name?")
+print("hello, " + name + "!")
+
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum is:", a + b)
+
+for i in range(1, 11):
+
+    print(i)
+
+ 
+a = 5
+
+b = 10
+
+c = 3
+
+print("Largest is", max(a, b, c))
+
+ 
